6ffd315c0800f4a6b398214ec7166d624f175c11
wanghongwei-dev <76191458+wanghongwei-dev@users.noreply.github.com>
2025-07-16 17:17:33 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // 允许外部访问（默认只允许 localhost）
     proxy: {
       '/socket.io': 'http://localhost:5000',
       '/download': 'http://localhost:5000'
-    },
-    host: true
+    }
   },
   build: {
     outDir: '../frontend/dist'
